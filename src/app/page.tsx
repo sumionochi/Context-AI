b1810d58962cddf356c6964f2748fef599818a36
sumionochi <aaditya.srivastava.connect@gmail.com>
2023-10-25 16:11:54 +0530

    trying to fixing the damn error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,3 @@
-import TypewriterTitle from '@/components/ui/TypewriterTitle';
-import TypewriterTitle2 from '@/components/ui/TypewriterTitle2';
-import { Button } from '@/components/ui/button';
-import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger }  from '@/components/ui/dropdown-menu';
-import { getAuthSession } from '@/lib/auth';
-import { User } from 'next-auth'
-import { ArrowRight, StickyNote } from 'lucide-react';
-import Link from 'next/link';
-import { redirect } from 'next/navigation';
-import React from 'react'
-import Tools from '@/components/Tools';
-import TypewriterTitle3 from '@/components/ui/TypewriterTitle3';
+import { Button } from "@/components/ui/button";
 
-type Props = {
-  user: User
-}
-
-const HomePage = async({user}: Props) => {
-  const session = await getAuthSession();
-  if(!session?.user){
-    return redirect("/display");
-  }
-  else{
-    return (
-      <h1 className='text-center'>/display</h1>
-    )
-  }
-}
-
-export default HomePage
-
+export default function Home() {return <Button>Hello world!</Button>;}
